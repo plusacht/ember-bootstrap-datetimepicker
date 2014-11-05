3a684b1853ae0fbdd08a25c63faa46053930b1b4
claudiocro <claudiocro@gmail.com>
2014-11-05 22:55:33 +0100

    add ember-cli-moment-show to blueprint bower package

--- blueprints/ember-cli-bootstrap-datetimepicker/index.js
+++ blueprints/ember-cli-bootstrap-datetimepicker/index.js
@@ -5,7 +5,9 @@ module.exports = {
     var that = this;
 
     return this.addBowerPackageToProject('eonasdan-bootstrap-datetimepicker').then(function() {
-        return that.addBowerPackageToProject('moment');
+        return that.addBowerPackageToProject('moment').then(function() {
+          return that.addBowerPackageToProject('ember-cli-moment-shim');
+      });
     });
   }
 };
