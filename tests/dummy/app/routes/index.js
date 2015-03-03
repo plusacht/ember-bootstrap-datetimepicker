b53a4ab8f19ba5d7ae1ac9dca50cd1cce155040f
claudiocro <claudiocro@gmail.com>
2015-03-03 11:56:05 +0100

    demonstrate fa icons with mixin

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -1,14 +1,16 @@
 import Ember from 'ember';
 import moment from 'moment';
+import FaIconsMixin from 'dummy/mixins/faicons';
 
 export default Ember.Route.extend({
 
 	model: function() {
-		return Ember.Object.create({
-			date1: moment(),
+		return Ember.Object.createWithMixins(FaIconsMixin,{
+    	date1: moment(),
 			date2: undefined,
 			mindate: moment("2014-11-01"),
 			maxdate: moment("2015-12-01"),
-			disabled:true});
+			disabled:true
+    });
 	}
 });
