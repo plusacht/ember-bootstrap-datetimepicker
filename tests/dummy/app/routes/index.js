e71941523dc9ec3413e132c18b682b2df746d800
claudiocro <claudiocro@gmail.com>
2015-03-09 22:06:34 +0100

    tabs

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -4,13 +4,13 @@ import FaIconsMixin from 'dummy/mixins/faicons';
 
 export default Ember.Route.extend({
 
-	model: function() {
-		return Ember.Object.createWithMixins(FaIconsMixin,{
-    	date1: moment(),
-			date2: undefined,
-			mindate: moment("2014-11-01"),
-			maxdate: moment("2015-12-01"),
-			disabled:true
+  model: function() {
+    return Ember.Object.createWithMixins(FaIconsMixin,{
+      date1: moment(),
+      date2: undefined,
+      mindate: moment("2014-11-01"),
+      maxdate: moment("2015-12-01"),
+      disabled:true
     });
-	}
+  }
 });
