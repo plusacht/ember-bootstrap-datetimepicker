2dc72e59c00ae1558cefad6ec4babf34cece483f
Claudio Romano <claudiocro@gmail.com>
2015-07-03 15:02:54 +0200

    add beta and canary tests but allow them to fail

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,6 +12,24 @@ module.exports = {
       resolutions: {
         'ember': 'release'
       }
+    },
+    {
+      name: 'ember-beta',
+      dependencies: {
+        'ember': 'components/ember#beta'
+      },
+      resolutions: {
+        'ember': 'beta'
+      }
+    },
+    {
+      name: 'ember-canary',
+      dependencies: {
+        'ember': 'components/ember#canary'
+      },
+      resolutions: {
+        'ember': 'canary'
+      }
     }
   ]
 };
