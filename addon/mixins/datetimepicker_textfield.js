b18b50f4d01fc42d56b165f38c0478df89b31b98
Bujorel Tecu <btecu@vikus.com>
2015-07-08 15:39:32 -0400

    Fix Ember.keys and template deprecations, cleaning

--- addon/mixins/datetimepicker_textfield.js
+++ addon/mixins/datetimepicker_textfield.js
@@ -1,19 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-
   /**
     Pick up exposed properties from parentView.textFieldOptions and bound them to this instance
-
   */
-  _initDateTimePickerTextField: Ember.on('init',function() {
+  _initDateTimePickerTextField: Ember.on('init', function() {
     var self = this;
-    var options = this.get("parentView.textFieldOptions");
-    this.set('classNames', this.get("parentView.textFieldClassNames"));
-    if(!Ember.isEmpty(options)) {
-      Ember.keys(options).forEach(function(key) {
-        Ember.defineProperty(self, options[key], Ember.computed("parentView."+key, function(){
-          return self.get("parentView."+key);
+    var options = this.get('parentView.textFieldOptions');
+    this.set('classNames', this.get('parentView.textFieldClassNames'));
+
+    if (!Ember.isEmpty(options)) {
+      Object.keys(options).forEach(function(key) {
+        Ember.defineProperty(self, options[key], Ember.computed('parentView.' + key, function() {
+          return self.get('parentView.' + key);
         }));
       });
     }
