141d02008e62d661f3194defe1bd2dd30d84758f
Claudio Romano <claudiocro@gmail.com>
2015-11-01 00:17:47 +0100

    test: test against bound 'dislabled' property

--- tests/integration/misc-test.js
+++ tests/integration/misc-test.js
@@ -2,6 +2,7 @@ import Ember from "ember";
 import hbs from 'htmlbars-inline-precompile';
 import startApp from '../helpers/start-app';
 import { test, moduleForComponent } from 'ember-qunit';
+import moment from 'moment';
 
 var App, component;
 
@@ -45,6 +46,8 @@ test("it shows the picker on input focus, then hides it after click outside", fu
   });
 });
 
+
+
 test("test yield", function(assert) {
   assert.expect(1);
   this.render(hbs`{{#bs-datetimepicker}}<label class="yieldholder">yield</label>{{input}}{{/bs-datetimepicker}}`);
@@ -64,6 +67,57 @@ test("test the useCurrent option default value is true", function(assert) {
   click($(".input-group-addon"));
 });
 
+test("test the useCurrent option set to false", function(assert) {
+  assert.expect(0);
+
+  this.on('handleDate', val => {
+    assert.ok(val);
+  });
+
+
+  this.render(hbs`{{bs-datetimepicker useCurrent=false updateDate='handleDate'}}`);
+  click($(".input-group-addon"));
+
+});
+
+test("test the useCurrent option set to true with initial value", function(assert) {
+  assert.expect(0);
+
+  this.on('handleDate', val => {
+    assert.ok(val);
+  });
+
+  this.set('date', moment('2015-11-01 09:30:00'));
+  this.render(hbs`{{bs-datetimepicker date=date updateDate='handleDate'}}`);
+  click($(".input-group-addon"));
+
+});
+
+test("test the useCurrent option set to true with initial date not rounded to the minute", function(assert) {
+  assert.expect(1);
+
+  this.on('handleDate', val => {
+    assert.ok(val);
+  });
+
+  this.set('date', moment('2015-11-01 09:30:01'));
+  this.render(hbs`{{bs-datetimepicker date=date updateDate='handleDate'}}`);
+  click($(".input-group-addon"));
+
+});
+
+test("test the forceDateOutput option", function(assert) {
+  assert.expect(1);
+
+  this.on('handleDate', val => {
+    assert.ok(val instanceof Date);
+  });
+
+  this.set('date', moment('2015-11-01 09:30:01'));
+  this.render(hbs`{{bs-datetimepicker forceDateOutput=true updateDate='handleDate'}}`);
+  click($(".input-group-addon"));
+
+});
 
 test("test the showClear option", function(assert) {
   assert.expect(1);
@@ -86,11 +140,109 @@ test("test the showClear option", function(assert) {
   });
 });
 
-test("test that non-computed properties are passed to the bootstrap-datetimepicker", function(assert) {
+test("test 'maxDate' bound property", function(assert) {
+  assert.expect(3);
+  let maxDate = moment().date(15);
+  this.set('maxDate', maxDate);
+  this.render(hbs`{{bs-datetimepicker maxDate=maxDate}}`);
+
+  andThen(() => {
+    click($(".input-group-addon"));
+  });
+
+  andThen(() => {
+    assert.ok(!$('*[data-day="'+maxDate.format('MM/DD/YYYY')+'"]').hasClass('disabled'), '15th of the month should be enabled');
+    assert.ok($('*[data-day="'+moment(maxDate).date(16).format('MM/DD/YYYY')+'"]').hasClass('disabled'), '16th of the month should be disabled');
+    click($(".input-group-addon"));
+  });
+
+  andThen(()=> {
+    this.set('maxDate', maxDate.date(16));
+  });
+
+  andThen(() => {
+    assert.ok(!$('*[data-day="'+moment(maxDate).date(16).format('MM/DD/YYYY')+'"]').hasClass('disabled'), '16th of the month should be enabled');
+  });
+});
+
+test("test 'minDate' bound property", function(assert) {
+  assert.expect(3);
+  let minDate = moment().date(15);
+  this.set('minDate', minDate);
+  this.render(hbs`{{bs-datetimepicker minDate=minDate}}`);
+
+  andThen(() => {
+    click($(".input-group-addon"));
+  });
+
+  andThen(() => {
+    assert.ok(!$('*[data-day="'+minDate.format('MM/DD/YYYY')+'"]').hasClass('disabled'), 'minDate should be enabled');
+    assert.ok($('*[data-day="'+moment(minDate).date(14).format('MM/DD/YYYY')+'"]').hasClass('disabled'), 'minDate - 1 day should be disabled');
+    click($(".input-group-addon"));
+  });
+
+  andThen(()=> {
+    this.set('minDate', minDate.date(14));
+  });
+
+  andThen(() => {
+    assert.ok(!$('*[data-day="'+moment(minDate).date(14).format('MM/DD/YYYY')+'"]').hasClass('disabled'), '14th of the month should be enabled');
+  });
+
+});
+
+// test("test 'disabledDates' bound property", function(assert) {
+//   assert.expect(6);
+//   let date1 = moment().date(15).startOf('day');
+//   let date2 = moment().date(16).startOf('day');
+//   let date3 = moment().date(17).startOf('day');
+//
+//   this.set('disabledDates', [date1, date3]);
+//   this.render(hbs`{{bs-datetimepicker disabledDates=disabledDates}}`);
+//
+//   andThen(() => {
+//     click($(".input-group-addon"));
+//   });
+//
+//   andThen(() => {
+//     assert.ok($('*[data-day="'+date1.format('MM/DD/YYYY')+'"]').hasClass('disabled'), 'date1 should be disabled');
+//     assert.ok(!$('*[data-day="'+date2.format('MM/DD/YYYY')+'"]').hasClass('disabled'), 'date2 should be enabled');
+//     assert.ok($('*[data-day="'+date3.format('MM/DD/YYYY')+'"]').hasClass('disabled'), 'date3 should be disabled');
+//     click($(".input-group-addon"));
+//   });
+//
+//   andThen(()=> {
+//     this.set('disabledDates', [date2]);
+//   });
+//
+//   andThen(() => {
+//     assert.ok(!$('*[data-day="'+date1.format('MM/DD/YYYY')+'"]').hasClass('disabled'), 'date1 should be enabled');
+//     assert.ok($('*[data-day="'+date2.format('MM/DD/YYYY')+'"]').hasClass('disabled'), 'date2 should be disabled');
+//     assert.ok(!$('*[data-day="'+date3.format('MM/DD/YYYY')+'"]').hasClass('disabled'), 'date3 should be enabled');
+//   });
+//
+// });
+
+test("test 'disabled' bound property", function(assert) {
+  assert.expect(2);
+
+  this.set('disabled', true);
+  this.render(hbs`{{bs-datetimepicker disabled=disabled}}`);
+
+  assert.ok(this.$('input').attr('disabled'), 'should be disabled');
+
+  Ember.run(()=> {
+    this.set('disabled', false);
+  });
+
+  assert.equal(this.$('input').attr('disabled'), undefined, 'no attribute disabled should be shown');
+});
+
+test("test 'noIcon' property", function(assert) {
   assert.expect(1);
 
-  this.set('localeName', 'en-IE');
-  this.render(hbs`{{bs-datetimepicker locale=localeName}}`);
+  this.render(hbs`{{bs-datetimepicker noIcon=true}}`);
 
-  assert.equal(this.$().length, 1);
+  assert.equal($(".input-group-addon").length, 0, 'no input-group-addon in html');
+
 });
